1c9514bcd750505d807dc4ad509a9a4803130db8
vasekva <vahonin_vasek@mail.ru>
2021-05-25 21:53:32 +0300

    added wordpress container

---
+++ .docker/wordpress/wp-config.php
@@ -0,0 +1,17 @@
+<?php
+define( 'DB_NAME', 'ft_server' );
+define( 'DB_USER', 'user' );
+define( 'DB_PASSWORD', 'password' );
+define( 'DB_HOST', 'localhost' );
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', '' );
+
+$table_prefix = 'wp_';
+
+define( 'WP_DEBUG', false );
+
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
+
+require_once ABSPATH . 'wp-settings.php';
