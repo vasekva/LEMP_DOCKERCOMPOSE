012155022443cd51a0c6d714da9854841b20aefd
Josephine Beregond <jberegon@un-h6.kzn.21-school.ru>
2021-06-06 22:43:15 +0300

    was added a few little change

--- srcs/requirements/wordpress/conf/wp.sh
+++ srcs/requirements/wordpress/conf/wp.sh
@@ -11,7 +11,7 @@ if [ ! -f /var/www/html/wp-config.php ]; then
   wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
   chmod +x wp-cli.phar
   mv wp-cli.phar /usr/local/bin/wp
-  cd var/www/html/
+  cd /var/www/html/
   wp core download --allow-root
   mv /var/www/wp-config.php /var/www/html/
   wp core install --allow-root \
