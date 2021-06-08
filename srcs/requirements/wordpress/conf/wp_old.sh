c0be7d41aa0fa2dd8c6aacd3d6005dd6b2b46f35
vasekva <vahonin_vasek@mail.ru>
2021-06-08 14:22:55 +0300

    Connected volumes wp and db

--- srcs/requirements/wordpress/conf/wp_old.sh
+++ srcs/requirements/wordpress/conf/wp_old.sh
@@ -6,12 +6,22 @@ sed -i "s/supervised no/supervised systemd/" "/etc/redis/redis.conf"
   chown -R www-data:www-data /var/www/*
   chmod -R 755 /var/www/*
 
-#if [ ! -f /var/www/html/wp-config.php ]; then
+#  cd /var/www/html && \
+#  wget https://wordpress.org/latest.tar.gz && \
+#	tar -xvzf latest.tar.gz && rm -rf latest.tar.gz && \
+#	cp /server_dir/wp-config.php ./wordpress/
+#	mv ./wordpress/ /var/www/html/
+
+if [ ! -d /var/www/html/wordpress ]; then
+  cd /var/www/html && \
+  wget https://wordpress.org/latest.tar.gz && \
+	tar -xvzf latest.tar.gz && rm -rf latest.tar.gz && \
+	cp /server_dir/wp-config.php ./wordpress/
 #  mkdir -p /var/www/html/
 #  wget https://wordpress.org/latest.tar.gz && \
 #	tar -xvzf latest.tar.gz && rm -rf latest.tar.gz && \
 #	cp wp-config.php wordpress/ && \
 #	mv wordpress /var/www/html/wordpress
-#fi
+fi
 service redis-server start
 exec "$@"
