b24541b3335e4b4a7ed9f4a9648af95d197d412f
vasekva <vahonin_vasek@mail.ru>
2021-06-09 20:25:37 +0300

    This works, but i need to change the condition on line 12 in ds.sh

--- srcs/requirements/mariadb/db.sh
+++ srcs/requirements/mariadb/db.sh
@@ -1,23 +1,16 @@
 #!/usr/bin/env bash
 #sed -i "s/bind-ad/\#bind-ad" "/etc/mysql/mariadb.conf.d/50-server.cnf"
 #sed -i "s/\#port /port /" "etc/mysql/mariadb.conf.d/50-server.cnf"
+chown -R root:root /var/lib/mysql
 chown -R mysql:mysql /var/lib/mysql
-#chown -R root:root /var/lib/mysql
 
-echo "Creating WP DB"
-if [ ! -d /var/lib/mysql/wp ]; then
-#  mkdir -p /var/run/mysqld
-#  mkfifo /var/run/mysqld/mysqld.sock
-#  touch /var/run/mysqld/mysqld.pid
-#  chown -R mysql /var/run/mysqld
+#  mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
+
+
+#echo "Creating WP DB"
+
+if [ ! -f /server_dir/50-server.cnf ]; then
   service mysql start
-#  mysql -u root -h 127.0.0.1 -p -P 3306
-#  mysql < create_new_user.sql
-#  mysqladmin -u root password $MYSQL_ROOT_PASSWORD
-#  mysql -u root -pnewpassword
-#  mysql -u root -e "SET PASSWORD FOR root@'localhost' = PASSWORD('cool42');"
-#  mysql -u root -p
-#  systemctl restart mysql
   mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE"
   mysql -u root -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'"
   mysql -u root -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%'"
@@ -25,14 +18,13 @@ if [ ! -d /var/lib/mysql/wp ]; then
   mysqladmin -u root password $MYSQL_ROOT_PASSWORD
   service mysql stop
 else
+  mv /server_dir/50-server.cnf /etc/mysql/mariadb.conf.d/50-server.cnf
   mkdir /var/run/mysqld
   mkfifo /var/run/mysqld/mysqld.sock
   touch /var/run/mysqld/mysqld.pid
-fi
-#  mkdir /var/run/mysqld
 #  mkfifo /var/run/mysqld/mysqld.sock
 #  touch /var/run/mysqld/mysqld.pid
+fi
   chown -R mysql /var/run/mysqld
-#  service mysql stop
 
-  exec "$@"
+exec "$@"
